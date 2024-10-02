9a5fe873dccdc90f1d15033183a33e55e4ddd585
Alfredo Aguilera <jose164788@gmail.com>
2024-10-02 15:45:41 -0700

    Add files via upload

---
+++ Primer parcial/Grados.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float numero, cel, far;
+    int opc = 0;
+
+    do {
+        cout << "===========Menu===========" << endl;
+        cout << "¿Qué es lo que quieres hacer?" << endl;
+        cout << "1) Grados Celsius a Fahrenheit" << endl;
+        cout << "2) Grados Fahrenheit a Celsius" << endl;
+        cout << "3) Salir" << endl;
+        cout << "¿Elige una opción: " << endl;
+        cin >> opc;
+        
+        if (opc > 3) {
+            cout << "Elige una opción entre 1 y 3" << endl;
+        }
+        
+        switch (opc) {
+            case 1: 
+                cout << "Introduzca los grados Celsius: " << endl;
+                cin >> numero;
+                far = (numero * 9 / 5) + 32;
+                cout << "La conversión de " << numero << " grados Celsius a Fahrenheit es: " << far << endl;
+            break;
+            case 2: 
+                cout << "Introduzca los grados Fahrenheit: " << endl;
+                cin >> numero;
+                cel = (numero - 32) * 5 / 9;
+                cout << "La conversión de " << numero << " grados Fahrenheit a Celsius es: " << cel << endl;
+            break;
+            case 3:
+                cout << "Saliendo..." << endl;
+            break;
+        }
+    } while (opc != 3);
+
+    return 0;
+}
